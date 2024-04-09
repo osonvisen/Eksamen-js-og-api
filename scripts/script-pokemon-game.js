1849a872ee5fe01f0586cf1aa1ec2e61469555d6
osonvisen <155632335+osonvisen@users.noreply.github.com>
2024-04-09 21:15:22 +0200

    Laget angrep og begynt på gameOver

--- scripts/script-pokemon-game.js
+++ scripts/script-pokemon-game.js
@@ -3,6 +3,7 @@ let pokemons = [
         name: "Pikachu",
         width: 64,
         height: 70,
+        image: "../../images/pikachu-right.png",
     },
     {
         name: "Charmander",
@@ -14,11 +15,13 @@ let pokemons = [
         name: "Squirtle",
         width: 107,
         height: 80,
+        image: "../../images/squirtle-right.png",
     },
     {
         name: "Bulbasaur",
         width: 80,
         height: 80,
+        image: "../../images/bulbasaur-right.png",
     },
 ];
 // Fetcher stats fra api og legger inn i pokemons
@@ -28,7 +31,6 @@ pokemons.forEach(async (pokemon) => {
     try {
         const response = await fetch(urlApi);
         const data = await response.json();
-        console.log(data.name);
         pokemon.health = Math.round(data.stats[0].base_stat);
         pokemon.attack = Math.round(data.stats[1].base_stat / 5);
         pokemon.speed = Math.round(data.stats[5].base_stat / 10);
@@ -36,11 +38,79 @@ pokemons.forEach(async (pokemon) => {
         console.error("Kunne ikke laste pokemoner!" + error);
     }
 });
+// Definerer områder som vi skal kunne kontrollere
 const playArea = document.querySelector(".play-area");
+const health1 = document.querySelector(".health1");
+const health2 = document.querySelector(".health2");
+const score = document.querySelector(".score");
+const attribution = document.querySelector(".attribution");
 
-setTimeout(theGame, 500);
+// Setter variabler vi trenger
+let player1choise;
+let player2choise;
+let chosingPlayer = 1; // Setter at spiller 1 er første
+
+// Skjuler noen elementer foreløpig
+score.style.visibility = "hidden";
+attribution.style.visibility = "hidden";
+
+setTimeout(chosePlayer, 200);
+function chosePlayer() {
+    playArea.innerHTML = "";
+    const viewPokemons = document.createElement("div"); // Inneholder alle pokemonene vi kan velge mellom
+    const choseDiv = document.createElement("div");
+    choseDiv.innerHTML = `<h2>Velg pokemon for spiller ${chosingPlayer}</h2>`;
+
+    const container = document.createElement("div");
+    container.style.display = "flex";
+    container.style.justifyContent = "space-between";
+
+    viewPokemons.classList.add("view-pokemons");
+    viewPokemons.style.display = "flex";
+    viewPokemons.style.flexDirection = "column";
+
+    pokemons.forEach((pokemon, index) => {
+        const pokemonDiv = document.createElement("div");
+        pokemonDiv.style.display = "flex";
+        pokemonDiv.style.flexDirection = "column";
+        const imageDiv = document.createElement("div");
+        const image = document.createElement("img");
+        image.src = pokemon.image;
+        image.style.height = "160px";
+        const textDiv = document.createElement("div");
+        textDiv.innerHTML = `${pokemon.name}<br>Health: ${pokemon.health}<br>Attack: ${pokemon.attack}<br>Speed: ${pokemon.speed}`;
+        imageDiv.append(image);
+        pokemonDiv.append(imageDiv, textDiv);
+        container.append(pokemonDiv);
+        viewPokemons.append(choseDiv, container);
+
+        image.addEventListener("click", (event) => {
+            image.removeEventListener("click", (event) => {});
+            if (chosingPlayer === 1) {
+                player1choise = index;
+
+                nextPlayer();
+            } else if (chosingPlayer === 2) {
+                player2choise = index;
+                theGame();
+            }
+        });
+    });
+
+    playArea.append(viewPokemons);
+}
+function nextPlayer() {
+    playArea.innerHTML = "";
+    chosingPlayer++;
+    setTimeout(chosePlayer, 400);
+}
+// setTimeout(theGame, 500);
 function theGame() {
     // Setter alle startvariabler
+    // chosingPlayer--; // Setter denne tilbake før neste spill
+    playArea.innerHTML = "";
+    score.style.visibility = "visible";
+    attribution.style.visibility = "visible";
     let Xspeed1 = 0;
     let Yspeed1 = 0;
     let moveLeft1 = false; // Setter at den skal stå i ro ved start
@@ -56,7 +126,14 @@ function theGame() {
     let moveDown2 = false;
 
     let attackPossible = false; // Det er bare mulig å angripe når man berører hverandre
-
+    let health1Status = pokemons[player1choise].health;
+    let health2Status = pokemons[player2choise].health;
+    function healthStatus() {
+        // Oppdaterer helsestatusen
+        console.log("Her skjedde det noe!");
+    }
+    health1.innerHTML = `${pokemons[player1choise].health} / ${health1Status}`;
+    health2.innerHTML = `${pokemons[player2choise].health} / ${health2Status}`;
     // Setter opp brettet
     let canvas = document.createElement("canvas");
     canvas.width = 1000;
@@ -64,37 +141,44 @@ function theGame() {
     let surface = canvas.getContext("2d");
     playArea.append(canvas);
     // Lager spill-objektene
-    let pokeObject1 = {
+    let playerObject1 = {
         x: 0,
         y: 0,
-        width: pokemons[1].width,
-        height: pokemons[1].height,
-        speed: pokemons[1].speed,
-        attack: pokemons[1].attack,
-        health: pokemons[1].health,
+        width: pokemons[player1choise].width,
+        height: pokemons[player1choise].height,
+        speed: pokemons[player1choise].speed,
+        attack: pokemons[player1choise].attack,
+        health: pokemons[player1choise].health,
+        name: pokemons[player1choise].name,
     };
-    let pokeObject2 = {
+    let playerObject2 = {
         x: 0,
         y: 0,
-        width: pokemons[0].width,
-        height: pokemons[0].height,
-        speed: pokemons[0].speed,
-        attack: pokemons[0].attack,
+        width: pokemons[player2choise].width,
+        height: pokemons[player2choise].height,
+        speed: pokemons[player2choise].speed,
+        attack: pokemons[player2choise].attack,
+        health: pokemons[player2choise].health,
+        name: pokemons[player2choise].name,
     };
-    let player1 = Object.create(pokeObject1);
+    let player1 = Object.create(playerObject1);
     player1.x = 100; // Startposisjon
     player1.y = 100;
     let image1 = new Image(); // Oppretter bildeobjekt
     image1.addEventListener("load", update, false); // Venter til bildet er lastet ferdig
-    image1.src = `../../images/${pokemons[1].name.toLowerCase()}-right.png`;
+    image1.src = `../../images/${pokemons[
+        player1choise
+    ].name.toLowerCase()}-right.png`;
 
     // Oppretter objekt av pokemon til spiller 2
-    let player2 = Object.create(pokeObject2);
+    let player2 = Object.create(playerObject2);
     player2.x = 836; //Startposisjon
     player2.y = 758;
     let image2 = new Image();
     image2.addEventListener("load", update, false);
-    image2.src = `../../images/${pokemons[0].name.toLowerCase()}-left.png`;
+    image2.src = `../../images/${pokemons[
+        player2choise
+    ].name.toLowerCase()}-left.png`;
 
     // Lytter etter tastetrykk
     window.addEventListener("keydown", function (e) {
@@ -126,10 +210,10 @@ function theGame() {
                 break;
             // Så for angrep
             case "q":
-                console.log("Player1 angrep");
+                attacks(player1.attack, player2.health);
                 break;
             case "-":
-                console.log("Player2 angrep");
+                attacks(player2.name, player1.name);
                 break;
         }
     });
@@ -172,16 +256,16 @@ function theGame() {
         player2.y += Yspeed2;
 
         if (moveUp1 && !moveDown1) {
-            Yspeed1 = -pokeObject1.speed;
+            Yspeed1 = -playerObject1.speed;
         }
         if (moveDown1 && !moveUp1) {
-            Yspeed1 = pokeObject1.speed;
+            Yspeed1 = playerObject1.speed;
         }
         if (moveLeft1 && !moveRight1) {
-            Xspeed1 = -pokeObject1.speed;
+            Xspeed1 = -playerObject1.speed;
         }
         if (moveRight1 && !moveLeft1) {
-            Xspeed1 = pokeObject1.speed;
+            Xspeed1 = playerObject1.speed;
         }
 
         if (!moveUp1 && !moveDown1) {
@@ -192,16 +276,16 @@ function theGame() {
         }
 
         if (moveUp2 && !moveDown2) {
-            Yspeed2 = -pokeObject2.speed;
+            Yspeed2 = -playerObject2.speed;
         }
         if (moveDown2 && !moveUp2) {
-            Yspeed2 = pokeObject2.speed;
+            Yspeed2 = playerObject2.speed;
         }
         if (moveLeft2 && !moveRight2) {
-            Xspeed2 = -pokeObject2.speed;
+            Xspeed2 = -playerObject2.speed;
         }
         if (moveRight2 && !moveLeft2) {
-            Xspeed2 = pokeObject2.speed;
+            Xspeed2 = playerObject2.speed;
         }
         if (!moveUp2 && !moveDown2) {
             Yspeed2 = 0;
@@ -234,8 +318,9 @@ function theGame() {
         if (player2.y + player2.height > canvas.height) {
             player2.y = canvas.height - player2.height;
         }
-        if (detectContact(pokeObject1, pokeObject2)) {
-            console.log("impakt");
+        // Sjekker om pokemonene er i kontakt med hverandre
+        if (detectContact(playerObject1, playerObject2)) {
+            console.log("Angrep");
             attackPossible = true;
         }
         render();
@@ -260,7 +345,7 @@ function theGame() {
         );
     }
 
-    function detectContact(pokeObject1, pokeObject2) {
+    function detectContact(playerObject1, playerObject2) {
         // Vi finner ut når spiller1 og spiller2 berører hverandre
         if (
             player2.x > player1.x &&
@@ -272,5 +357,14 @@ function theGame() {
         } else {
             return false;
         }
+    }
+    function attacks(attacker, defender) {
+        // Når en av spillerne angriper:
+        if (attackPossible) {
+            console.log(attacker + " angrep " + defender);
+        }
+    }
+    function gameOver() {
+        window.location.reload();
     }
 }
